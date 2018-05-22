88ee8ad4bbd1b467b788ba5955ef56b3108c457e
Pawel Bus <pawelbus.dev@outlook.com>
2018-05-22 11:01:02 +0200

    Add function getJoke()

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@ var url = 'http://api.icndb.com/jokes/random',
 button.addEventListener('click', function() {
   getJoke();
 });
+
+function getJoke() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.addEventListener('load', function() {
+    var response = JSON.parse(xhr.response);
+    paragraph.innerHTML = response.value.joke;
+  });
+  xhr.send();
+}
